bd3de8ab9b13cc7873f2eb080e8233d2cf5bb984
Ali G <aliguduz26@gmail.com>
2024-03-22 02:17:06 +0300

    First build

---
+++ wp-app/wp-content/plugins/voting/core/template-parts/voting-widget.php
@@ -0,0 +1,9 @@
+<section id="voting">
+    <div class="container">
+        <span>Was this Article Helpful?</span>
+        <div class="buttons">
+            <a href="#">YES</a>
+            <a href="#">NO</a>
+        </div>
+    </div>
+</section>
---
+++ wp-app/wp-content/plugins/voting/voting.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Custom Voting Plugin
+Description: This plugin provides voting options to every post or selected posts.
+Version: 1.0
+Author: Ali G. 
+License: GPL2
+*/
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+require_once('core/functions.php');
+
+
+?>
